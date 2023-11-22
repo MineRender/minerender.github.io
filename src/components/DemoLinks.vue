30ea1e541bb16b2e4b71af562cd53408d3ce10d1
inventivetalent <mail@inventivetalent.org>
2023-11-22 10:10:10 +0100

    .

--- src/components/DemoLinks.vue
+++ src/components/DemoLinks.vue
@@ -1,10 +1,22 @@
 <template>
-    <v-row justify="center" class="text-center">
-        <v-col cols="1">
-            <v-btn :href="'https://minerender.github.io/' + name" target="_blank">Demo</v-btn>
+    <v-row justify="center">
+        <v-col cols="2" class="text-right">
+            <v-btn
+                color="primary"
+                prepend-icon="mdi-play-box-outline"
+                :href="'https://minerender.github.io/' + name"
+                target="_blank"
+            >
+                Demo
+            </v-btn>
         </v-col>
-        <v-col cols="1">
-            <v-btn :href="'https://codesandbox.io/p/github/MineRender/' + name" target="_blank">CodeSandbox</v-btn>
+        <v-col cols="2" class="text-left">
+            <v-btn
+                color="secondary"
+                prepend-icon="mdi-code-braces"
+                :href="'https://codesandbox.io/p/github/MineRender/' + name + '?file=%2Findex.ts'"
+                target="_blank"
+            >Edit</v-btn>
         </v-col>
     </v-row>
 </template>
